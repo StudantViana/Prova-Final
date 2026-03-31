a6c55b438da36607447534bd2c5b5ddea362726f
StudantViana <guilherme.fontes@ba.estudante.senai.br>
2026-03-31 16:27:18 -0300

    prova final

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 const app = express()
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 
